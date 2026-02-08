6a6e10b30bc155ee239766ca585cd6d415d6dee4
Chris Johnson <animastralis@gmail.com>
2026-02-08 00:32:34 -0500

    Added arrow key movement as option

--- assets/ball.gd
+++ assets/ball.gd
@@ -9,9 +9,23 @@ func _input(event: InputEvent) -> void:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
 				boost()
+	
+	# arrow key movement
+	if event.is_action_pressed("boost_up"):
+		boost_dir(Vector2.UP)
+	if event.is_action_pressed("boost_down"):
+		boost_dir(Vector2.DOWN)
+	if event.is_action_pressed("boost_left"):
+		boost_dir(Vector2.LEFT)
+	if event.is_action_pressed("boost_right"):
+		boost_dir(Vector2.RIGHT)
 
 
 func boost() -> void:
 	var target := get_global_mouse_position()
 	var boost_direction := (target - global_position).normalized()
 	apply_central_impulse(boost_direction * boost_strength)
+
+
+func boost_dir(direction: Vector2) -> void:
+	apply_central_impulse(direction * boost_strength)
